9e574b942ebdbe0a1a1fa136f7bd01ed0a7a00c5
Isaac Kellogg <keldtdev@gmail.com>
2026-08-25 14:12:23 -0400

    Add comment explaining SpoilerComponent usage.

--- src/spoiler/SpoilerComponent.jsx
+++ src/spoiler/SpoilerComponent.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import './SpoilerComponent.css'
 
-
+/**
+ * Usage: `<InlineSpoiler>Hidden content, shown when previewMessage text is clicked.</InlineSpoiler>`  
+ * Optionally, set previewMessage to a custom string or JSX. When unset, defaults to "(spoiler)".  
+ * Hidden content can be text or markdown or HTML/JSX or whatever.
+ */
 export default function InlineSpoiler({previewMessage, children}) {
     const [isExpanded, setIsExpanded] = useState(false);
     const defaultPreviewMessage = "(spoiler)"
